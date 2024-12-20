ece908383658f9ca3d9145f7b5600b8921350de3
AlexandreCANOVAS <alexandre.oclock31@gmail.com>
2024-12-20 12:43:10 +0100

    read.me à jour 5
    ajout feature ticket
    
    test

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const clotureCommand = require('./clotureCommand.js');
 const voteTopServeur = require('./voteTopServeur');  // Importer la fonctionnalité de vote
 const guildMemberEvents = require('../BOT-WOLF-V2-RDR/events/memberAddRemove.js');
 const roleReaction = require('./roleReaction');
-
-
+const acceptCommand = require('./accept'); // Import de la commande 'accepter' (dans le même répertoire)
 
 const TOKEN = process.env.DISCORD_TOKEN; // Charge le token depuis les variables d'environnement
 const PREFIX = '-'; // Préfixe pour les commandes
@@ -18,7 +17,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,              // Permet d'interagir avec les serveurs
     GatewayIntentBits.GuildMessages,       // Permet de lire et répondre aux messages
     GatewayIntentBits.MessageContent,      // Permet de lire le contenu des messages
-    GatewayIntentBits.GuildMembers,         // Permet de gérer les rôles et membres du serveur
+    GatewayIntentBits.GuildMembers,        // Permet de gérer les rôles et membres du serveur
     GatewayIntentBits.GuildMessageReactions  // Pour gérer les réactions
   ],
 });
@@ -27,35 +26,30 @@ const client = new Client({
 client.once('ready', () => {
   console.log(`Bot connecté en tant que ${client.user.tag}`);
   voteTopServeur.startRecurringMessages(client);  // Démarrer l'envoi récurrent des messages
-  // Démarrer la fonctionnalité de rôle avec réaction
   roleReaction.sendMessage(client);  // Envoie le message avec la réaction
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
   try {
-    // Ignorer les réactions des bots
     if (user.bot) return;
 
-    // Vérifie si le message provient d'un serveur
     if (!reaction.message.guild) return;
 
-    const member = await reaction.message.guild.members.fetch(user.id); // Récupère le membre dans le cache ou depuis l'API
+    const member = await reaction.message.guild.members.fetch(user.id);
 
     if (!member) {
       console.log(`Membre introuvable pour l'utilisateur ${user.tag}`);
       return;
     }
 
-
     if (reaction.emoji.name === '📝') {
       const role = reaction.message.guild.roles.cache.find(r => r.name === '📝 | RP écrit');
       if (role) {
-        await member.roles.add(role); // Attribuer le rôle
+        await member.roles.add(role);
         console.log(`${user.tag} a reçu le rôle 📝 | RP écrit`);
 
-        // Envoi d'un message privé pour confirmer l'attribution du rôle
         try {
-          await member.send(`🎉 Bonjour ${user.username}, vous avez reçu le rôle **📝 | RP écrit** ! 🎉\n\nVous pouvez maintenant accéder à tout le contenu RP écrit et commencer à participer pleinement au rôle-play. Merci de votre engagement sur le serveur !`);
+          await member.send(`🎉 Bonjour ${user.username}, vous avez reçu le rôle **📝 | RP écrit** ! 🎉`);
           console.log(`Message privé envoyé à ${user.tag}`);
         } catch (error) {
           console.error(`Erreur lors de l'envoi du message privé à ${user.tag}:`, error);
@@ -70,25 +64,16 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 
 client.on('guildMemberAdd', (member) => {
-  guildMemberEvents.execute(member, 'add');  // Appel de l'événement "add"
+  guildMemberEvents.execute(member, 'add');
 });
 
 client.on('guildMemberRemove', (member) => {
-  guildMemberEvents.execute(member, 'remove');  // Appel de l'événement "remove"
+  guildMemberEvents.execute(member, 'remove');
 });
 
 // Écoute des messages entrants
 client.on('messageCreate', async (message) => {
-  console.log("Message reçu :");
-  console.log(`Auteur : ${message.author.tag}`);
-  console.log(`Contenu brut : "${message.content}"`);
-  console.log(`Type : ${message.type}`);
-
-  
-
   if (message.author.bot) return;
-
-  // Supprime la déclaration de `prefix` ici
 
   if (message.content.startsWith(`${PREFIX}clôture`)) {
     await clotureCommand.execute(message);
@@ -98,89 +83,17 @@ client.on('messageCreate', async (message) => {
     await lancementCommand.execute(message);
   }
 
-  // Vérifie si le message commence par le préfixe
-  if (!message.content.startsWith(PREFIX)) return;
-
-  // Divise le message en commande et arguments
-  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-   // Commande 'session'
-   if (command === 'session') {
-    // Supprime le message contenant la commande
-    try {
-      await message.delete(); // Cette ligne supprime le message contenant la commande
-      // Après la suppression, le message est déjà traité, donc la commande est exécutée ici
-      return sessionCommand.execute(message); // Exécute la commande
-    } catch (error) {
-      console.error("Erreur lors de la suppression du message : ", error);
-    }
+  if (message.content.startsWith(`${PREFIX}proposition session`)) {
+    await propositionSessionCommand.execute(message);
   }
 
-   // Commande 'proposition session'
-   if (message.content.startsWith(`${PREFIX}proposition session`)) {
-    propositionSessionCommand.execute(message);
+  // Commande acceptée
+  if (message.content.startsWith(`${PREFIX}accepter`)) {
+    await acceptCommand.execute(message); // Appelle la fonction de la commande 'accepter'
   }
 
-  // Commande 'accepter'
-  if (command === 'accepter') {
-    const member = message.mentions.members.first();
-
-    if (!member) {
-      return message.reply('Veuillez mentionner un membre valide pour accepter la candidature.');
-    }
-
-    const roleImmigre = message.guild.roles.cache.find((role) => role.name === '🗺| Immigré');
-    const roleResident = message.guild.roles.cache.find((role) => role.name === '🏠| Résident');
-    const roleRPVocal = message.guild.roles.cache.find((role) => role.name === '🎙 | RP vocal');
-
-    // Vérification des rôles existants sur le serveur
-    if (!roleImmigre || !roleResident || !roleRPVocal) {
-      return message.reply('Les rôles "immigré", "résident" ou "RP vocal" sont introuvables sur ce serveur.');
-    }
-
-    try {
-      // Retirer le rôle "immigré" et attribuer les rôles "résident" et "RP vocal"
-      if (member.roles.cache.has(roleImmigre.id)) {
-        await member.roles.remove(roleImmigre);
-      }
-      await member.roles.add(roleResident);
-      await member.roles.add(roleRPVocal);
-
-      // Envoi d'un message privé à l'utilisateur concerné
-      await member.send(
-        `Bonjour, 
-
-Vous venez d'être accepté dans le serveur ! :grin: 
-Félicitations à vous ! :partying_face: 
-
-:page_with_curl:  - Vous pouvez dès à présent faire le tour des catégories pour prendre en compte tout son contenu. 
-
-:money_with_wings:  - Familiarisez-vous avec le bot dans la catégorie Compte et faites un !money dans le premier salon pour ouvrir votre compte en banque.
-
-Si vous avez des questions, nous restons bien entendu disponibles, soit via un ticket si vous êtes timides, soit directement dans le salon discussion :smile: 
-
-Nous sommes tous là pour vous répondre.
-
-:warning: Pour votre première session, pensez à vous rendre au bureau des shérifs afin de vous faire recenser. 
-Passez ensuite à l'écurie ou au ranch pour recenser votre cheval. :warning: 
-
-A bientôt en RP sur **${message.guild.name}**.`
-      );
-
-      // Confirmation dans le salon
-      message.channel.send(
-        `Une candidature a été acceptée et les rôles \`🏠 Résident\` et \`🎙 RP vocal\` ont été attribués. :white_check_mark:`
-      );
-    } catch (error) {
-      console.error(error);
-      message.reply("Une erreur est survenue lors de l'attribution des rôles.");
-    }
-  }
-
-  // Commande 'session' - Appel de la nouvelle fonctionnalité
-  if (command === 'session') {
-    sessionCommand.execute(message);
+  if (message.content.startsWith(`${PREFIX}session`)) {
+    await sessionCommand.execute(message);
   }
 });
 
